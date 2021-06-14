b8c63e72dc34c27bdace98957e73fb9f87f38941
Nishay Madhani <nshmadhani@gmail.com>
2021-06-14 18:40:33 +0530

    Initial commit

---
+++ config.js
@@ -0,0 +1,43 @@
+'use strict';
+
+module.exports = {
+  url: 'https://victorzhou.com',
+  title: 'victorzhou.com',
+  subtitle:
+    'Software Engineer. I blog about web development, machine learning, and more topics.',
+  copyright: '© Victor Zhou 2021',
+  postsPerPage: 8,
+  googleAnalyticsId: 'UA-58664433-1',
+  menu: [
+    {
+      label: 'Blog',
+      path: '/',
+    },
+    {
+      label: 'About',
+      path: '/about/',
+    },
+    {
+      label: 'Tags',
+      path: '/tags/',
+    },
+    {
+      label: 'More Posts',
+      path: '/posts/',
+    },
+  ],
+  author: {
+    name: 'Victor Zhou',
+    photo: '/photo2.png',
+    photoLarge: '/photo2-large.png',
+    bio:
+      'Software Engineer. I blog about <a href="/tag/web-development/">web development</a>, <a href="/tag/machine-learning/">machine learning</a>, and <a href="/tags/">more topics</a>.',
+    contacts: {
+      email: 'vzhou842@gmail.com',
+      twitter: 'victorczhou',
+      github: 'vzhou842',
+      rss: '/rss.xml',
+    },
+  },
+  previewImage: '/preview.png'
+};
